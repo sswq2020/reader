fe7b1a6322159ee4b585611f6e45016c6153d8a0
shiswq@hletong.com <shiswq@hletong.com>
2020-03-24 07:49:13 +0800

    feat: 🎸 封装fly请求库,和axios差不多

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,27 @@
+/** *返回fly实例的工厂方法**/
 export function createFly () {
   if (mpvuePlatform === 'wx') {
     const Fly = require('flyio/dist/npm/wx')
     return new Fly()
   }
   return null
+}
+
+export function handleError (err) {
+  console.log(err)
+}
+
+export function get (url, params = {}) {
+  return new Promise((resolve, reject) => {
+    const fly = createFly()
+    if (fly) {
+      fly.get(url, params).then(response => {
+        resolve(response)
+      }).catch((err) => {
+        handleError(err)
+        console.log(err)
+        reject(err)
+      })
+    }
+  })
 }
