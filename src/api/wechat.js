4e232ccc604526c51c7c40d0bf2210e73334ed49
shiswq@hletong.com <shiswq@hletong.com>
2020-03-31 15:53:29 +0800

    perf: ⚡️ 将微信2个接口改造成promise,写起来更好理解

--- src/api/wechat.js
+++ src/api/wechat.js
@@ -1,14 +1,34 @@
-export function getSetting(auth, onSuccess, onFail) {
-  mpvue.getSetting({
-    success(res) {
-      if (res.authSetting[`scope.${auth}`]) {
-        onSuccess(res)
-      } else {
-        onFail(res)
+export function getSetting(auth) {
+  return new Promise((resolve, reject) => {
+    mpvue.getSetting({
+      success(res) {
+        if (res.authSetting[`scope.${auth}`]) {
+          resolve(res)
+        } else {
+          reject(res)
+        }
+      },
+      fail(res) {
+        console.log(res)
       }
-    },
-    fail(res) {
-      console.log(res)
-    }
+    })
+  })
+}
+
+export function getUserInfo() {
+  return new Promise((resolve, reject) => {
+    mpvue.getUserInfo({
+      success(res) {
+        const {userInfo} = res
+        if (userInfo) {
+          resolve(userInfo)
+        } else {
+          reject(res)
+        }
+      },
+      fail(res) {
+        console.log(res)
+      }
+    })
   })
 }
