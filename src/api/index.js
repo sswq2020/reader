105c8ef1f3339eea00265fe12eca12d4ea23e096
shiswq@hletong.com <shiswq@hletong.com>
2020-04-01 11:48:52 +0800

    feat: 🎸 接口层级最好全都promise化,这样代码写起来方便很多

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,10 @@ import { fetch } from 'utils/request'
 
 const BASE_URL = 'https://test.youbaobao.xyz:18081'
 
+const APP_ID = 'wx7890931dc5e138bf'
+
+const APP_SECRET = '23d8235a176e4e63ddcb37f47e5757e6'
+
 export function getHomeData(openId) {
   return fetch(`${BASE_URL}/book/home/v2`, {openId})
 }
@@ -16,4 +20,12 @@ export function getFreeRead() {
 
 export function getHotBook() {
   return fetch(`${BASE_URL}/book/home/hotBook/v2`)
+}
+
+export function getOpenId(code) {
+  return fetch(`${BASE_URL}/openId/get`, {
+    appId: APP_ID,
+    secret: APP_SECRET,
+    code
+  })
 }
--- src/api/wechat.js
+++ src/api/wechat.js
@@ -1,3 +1,5 @@
+import { getOpenId } from 'api/index'
+
 export function getSetting(auth) {
   return new Promise((resolve, reject) => {
     mpvue.getSetting({
@@ -31,4 +33,46 @@ export function getUserInfo() {
       }
     })
   })
+}
+/** *设置微信存储数据**/
+export function setStorageSync(key, data) {
+  mpvue.setStorageSync(key, data)
+}
+/** *从微信存储中获得数据**/
+export function getStorageSync(key) {
+  return mpvue.getStorageSync(key)
+}
+
+/** *微信登陆,拿到code,返回promise**/
+export function login() {
+  return new Promise((resolve, reject) => {
+    mpvue.login({
+      success(res) {
+        if (res && res.code) {
+          resolve(res.code)
+        } else {
+          reject(res)
+        }
+      },
+      fail(err) {
+        console.log(err)
+      }
+    })
+  })
+}
+
+/**
+ * @description 获取用户的openId
+ * @param {Function} cb 获取openId后的回调函数,可以没有
+ * */
+export async function getUserOpenId(cb) {
+  try {
+    const code = await login()
+    const res = await getOpenId(code)
+    const { data: { openid } } = res
+    setStorageSync('openId', openid)
+    cb && cb(openid)
+  } catch (error) {
+    console.log(error)
+  }
 }
