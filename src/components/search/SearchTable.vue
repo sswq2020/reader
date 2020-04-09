0ff88a6f7099e101e881f277be09c8e30c3244ab
shiswq@hletong.com <shiswq@hletong.com>
2020-04-09 14:52:43 +0800

    feat: 🎸 SearchTable组件完成

---
+++ src/components/search/SearchTable.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="search-table-wrapper">
+    <div class="search-table-inner">
+      <div class="search-table-book"
+           v-for="(book,index) in data"
+           :key="index"
+           @click="onClick"
+      >
+        <div class="book-img-wrapper">
+          <div class="book-img">
+            <ImageView
+              :src="book.cover"
+            />
+          </div>
+        </div>
+        <div class="book-info-wrapper">
+          <div class="book-title">{{book.title}}</div>
+          <div class="book-author">{{book.author}}</div>
+          <div class="book-category">{{book.categoryText}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ImageView from 'components/base/ImageView'
+export default {
+  name: 'SearchTable',
+  components: {
+    ImageView
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('onClick')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-table-wrapper{
+  padding: 0 16px;
+  .search-table-inner{
+    .search-table-book{
+      display: flex;
+      align-items: center;
+      .book-img-wrapper{
+        .book-img{
+          width:47px;
+          height:68.5px;
+        }
+      }
+      .book-info-wrapper{
+        margin-left:15.5px;
+        width:80%;
+        .book-title{
+          font-size: 16px;
+          color: #333;
+          line-height:22.5px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+        .book-author{
+          font-size: 13px;
+          color: rgba(0,0,0,0.65);
+          line-height: 18px;
+        }
+        .book-category{
+          font-size: 13px;
+          color: rgba(0,0,0,0.45);
+          line-height: 18px;
+        }
+
+      }
+    }
+  }
+}
+</style>
--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -12,29 +12,20 @@
       title="计算机科学"
       sub-title="类别"
     ></SearchItem>
-    <SearchItem
-      icon="apps-o"
-      title="计算机科学"
-      sub-title="类别"
-    ></SearchItem>
-    <SearchItem
-      icon="apps-o"
-      title="计算机科学"
-      sub-title="类别"
-    ></SearchItem>
-
-
+    <SearchTable :data="list"></SearchTable>
   </div>
 </template>
 
 <script>
 import TagGroup from 'components/base/TagGroup'
 import SearchItem from 'components/search/SearchItem'
+import SearchTable from 'components/search/SearchTable'
 export default {
   name: 'search',
   components: {
     TagGroup,
-    SearchItem
+    SearchItem,
+    SearchTable
   },
   data() {
     return {
@@ -48,6 +39,58 @@ export default {
         'ddd',
         'ddd',
         'ddd'
+      ],
+      list: [
+        {
+          'id': 19,
+          'fileName': '2018_Book_AutonomousControlForAReliableI',
+          'cover': 'https://www.youbaobao.xyz/book/res/img/ComputerScience/978-3-319-90415-3_CoverFigure.jpg',
+          'title': 'Autonomous Control for a Reliable Internet of Services',
+          'author': 'Ivan Ganchev',
+          'publisher': 'Springer International Publishing',
+          'bookId': '2018_Book_AutonomousControlForAReliableI',
+          'category': 1,
+          'categoryText': 'ComputerScience',
+          'language': 'en',
+          'rootFile': 'OEBPS/package.opf'
+        }, {
+          'id': 20,
+          'fileName': '2018_Book_ComputerAidedVerification',
+          'cover': 'https://www.youbaobao.xyz/book/res/img/ComputerScience/978-3-319-96142-2_CoverFigure.jpg',
+          'title': 'Computer Aided Verification',
+          'author': 'Hana Chockler',
+          'publisher': 'Springer International Publishing',
+          'bookId': '2018_Book_ComputerAidedVerification',
+          'category': 1,
+          'categoryText': 'ComputerScience',
+          'language': 'en',
+          'rootFile': 'OEBPS/package.opf'
+        },
+        {
+          'id': 19,
+          'fileName': '2018_Book_AutonomousControlForAReliableI',
+          'cover': 'https://www.youbaobao.xyz/book/res/img/ComputerScience/978-3-319-90415-3_CoverFigure.jpg',
+          'title': 'Autonomous Control for a Reliable Internet of Services',
+          'author': 'Ivan Ganchev',
+          'publisher': 'Springer International Publishing',
+          'bookId': '2018_Book_AutonomousControlForAReliableI',
+          'category': 1,
+          'categoryText': 'ComputerScience',
+          'language': 'en',
+          'rootFile': 'OEBPS/package.opf'
+        }, {
+          'id': 20,
+          'fileName': '2018_Book_ComputerAidedVerification',
+          'cover': 'https://www.youbaobao.xyz/book/res/img/ComputerScience/978-3-319-96142-2_CoverFigure.jpg',
+          'title': 'Computer Aided Verification',
+          'author': 'Hana Chockler',
+          'publisher': 'Springer International Publishing',
+          'bookId': '2018_Book_ComputerAidedVerification',
+          'category': 1,
+          'categoryText': 'ComputerScience',
+          'language': 'en',
+          'rootFile': 'OEBPS/package.opf'
+        }
       ]
     }
   },
